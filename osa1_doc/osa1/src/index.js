9ca572e49c503e4f945f862f180d6dc9340daa4b
vaaPo <ripraptiptop@gmail.com>
2018-10-30 09:40:15 +0200

    step1-simplify

--- osa1_doc/osa1/src/index.js
+++ osa1_doc/osa1/src/index.js
@@ -1,12 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+//step1-simplify import './index.css';
+//step1-simplify import App from './App';
+//step1-simplify import * as serviceWorker from './serviceWorker';
+
+//step1-simplify Voit poistaa tiedostot
+// App.js, App.css, App.test.js, logo.svg ja registerServiceWorker.js
+
+const App = () => (
+    <div>
+      <p>Hello world</p>
+    </div>
+  )
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+
+//step1-simplify serviceWorker.unregister();
