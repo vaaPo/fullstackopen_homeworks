ecd66c321db9b37b0a6dfbb1074922d9cf9e6107
vaaPo <ripraptiptop@gmail.com>
2018-10-30 10:58:08 +0200

    step1, step2, step3 combined

--- osa1_doc/osa1/src/index.js
+++ osa1_doc/osa1/src/index.js
@@ -7,45 +7,47 @@ import ReactDOM from 'react-dom';
 //step1-simplify Voit poistaa tiedostot
 // App.js, App.css, App.test.js, logo.svg ja registerServiceWorker.js
 
-//step1 const App = () => (
-//step1     <div>
-//step1       <p>Hello world</p>
-//step1     </div>
-//step1   )
-
-//step2-add_consts
-//step2 const App = () => {
-//step2     const now = new Date()
-//step2     const a = 10
-//step2     const b = 20
-//step2     return (
-//step2       <div>
-//step2         <p>Hello world, it is {now.toString()}</p>
-//step2         <p>{a} plus {b} is {a + b}</p>
-//step2       </div>
-//step2     )
-//step2   }
-  
-//step3-several_components
 const Hello = () => {
     return (
-      <div>
-        <p>Hello world</p>
-      </div>
+        <div>
+            <p>Hello world</p>
+        </div>
     )
-  }
-  
-  const App = () => {
+}
+
+
+//step4-user_components in App2
+const App = () => {
+    const now = new Date() //step2-add_consts
+    const a = 10           //step2-add_consts
+    const b = 20           //step2-add_consts
+
     return (
-      <div>
-        <h1>Greetings</h1>
-        <Hello />
-      </div>
+        <div id="step1">
+            <h1>Greetings step1</h1>
+            //step1-simplify code
+            <p>Hello world</p>
+            <div id="step2">
+                <h2>Greetings step2</h2>
+                //step2-add_consts
+                <p>Hello world, it is {now.toString()}</p>
+                <p>{a} plus {b} is {a + b}</p>
+                <div id="step3">
+                    <h3>Greetings step3</h3>
+                    //step3-use_components
+                    <Hello />
+                    <Hello />
+                    <Hello />
+                </div>
+            </div>
+        </div>
     )
-  }
+}
 
 
+//ReactDOM.render(<App />, document.getElementById('root'));
 ReactDOM.render(<App />, document.getElementById('root'));
+
 //step1-simplify Hello world
 
 // If you want your app to work offline and load faster, you can change
