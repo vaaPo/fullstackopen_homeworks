a83d1a1d57cf4bba17e0b412844f825b7217e88b
vaaPo <ripraptiptop@gmail.com>
2018-11-03 18:33:41 +0200

    javascript console, vars, tables, objects, arrow, function

--- osa1_doc/osa1/src/index.js
+++ osa1_doc/osa1/src/index.js
@@ -56,6 +56,121 @@ const App = () => {
     const b = 20           //step2-add_consts
     const nimi = 'Pekka'    //step5-2props
     const ika = 10          //step5-2props
+    const x = 1             //step8-javascript-vars-console
+    let y = 5               //step8-javascript-vars-console
+    console.log('step8-javascript-vars-console')
+    console.log(x, y)  // step8-javascript-vars-console tulostuu 1, 5
+    y += 10
+    console.log(x, y)  // step8-javascript-vars-console tulostuu 1, 15
+    y = 'teksti'
+    console.log(x, y)  // step8-javascript-vars-console tulostuu 1, teksti
+    //x = 4            // step8-javascript-vars-consoleaiheuttaa virheen, koska x on const (vakio)
+
+    console.log('step9-javascript-table')
+    const t = [1, -1, 3]    //step9-javascript-table
+    t.push(5)
+    console.log(t.length)  // tulostuu 4
+    console.log(t[1])      // tulostuu -1
+    t.forEach((luku) => {
+        console.log(luku)    // tulostuu 1, -1, 3 ja 5 omille riveilleen
+    })
+    t[6] = 99
+    console.log(t)         // tulostuu [ 1, -1, 3, 5, <2 empty items>, 99 ]
+
+    console.log('step10-javascript-map')
+    const t2 = [1, 2, 3, 4]
+
+    const m1 = t2.map((luku) => luku * 2)
+    console.log(m1) // tulostuu [2, 4, 6, 8]
+    
+    const m2 = t2.map((luku) => '<li>' + luku + '</li>')
+    console.log(m2) // tulostuu [ '<li>1</li>', '<li>2</li>', '<li>3</li>', '<li>4</li>' ]
+    
+    console.log('step11-javascript-destructuroiva-sijoitus')
+    const t3 = [1, 2, 3, 4, 5]
+    const [eka, toka, ...loput] = t3
+    console.log(eka, toka)      // tulostuu 1, 2
+    console.log(loput)          // tulostuu [3, 4 ,5]
+
+    console.log('step12-olioliteraalit')
+    const olio1 = {
+        nimi: 'Arto Hellas',
+        ika: 35,
+        koulutus: 'Filosofian tohtori'
+      }
+      
+      const olio2 = {
+        nimi: 'Full Stack -websovelluskehitys',
+        taso: 'aineopinto',
+        laajuus: 5
+      }
+      
+      const olio3 = {
+        nimi: {
+          etunimi: 'Jami',
+          sukunimi: 'Kousa'
+        },
+        arvosanat: [2, 3, 5, 3],
+        laitos: 'TKTL'
+      }
+    console.log(olio1.nimi)          // tulostuu Arto Hellas
+    const kentanNimi = 'ika'
+    console.log(olio1[kentanNimi])   // tulostuu 35
+
+    console.log('step13-oliolle-lennosta-fields')
+    olio1.osoite = 'Tapiola'
+    olio1['salainen numero'] = 12341
+
+    const kentanNimi2 = 'osoite'
+    console.log(olio1[kentanNimi2])   // 
+    const kentanNimi3 = 'salainen numero'
+    console.log(olio1[kentanNimi3])   // 
+
+    console.log('step14-arrow-functions')
+
+    const summa = (p1, p2) => {
+      console.log(p1)
+      console.log(p2)
+      return p1 + p2
+    }
+    const vastaus = summa(1,5)
+    console.log(vastaus)
+
+    console.log('step15-arrow-functions-short-without-curly')
+    const neliox = p => {
+        console.log(p)
+        return p * p
+      }
+    console.log(neliox(3))
+
+    const nelio = p => p * p
+    console.log(nelio(4))
+
+    const tn=[1,2,3]
+    tn.forEach((luku) => {
+        console.log(luku)    
+    })
+
+    const tnelio=tn.map(p => p*p)
+    tnelio.forEach((luku) => {
+        console.log(luku)    
+    })
+ 
+    function tulo(a, b) {
+        return a * b
+      }
+      
+    const vastaus2 = tulo(2, 6)
+    console.log(vastaus2)      
+
+    const keskiarvo = function(a, b) {
+        return (a + b) / 2
+      }
+      
+    const vastaus3 = keskiarvo(2, 5)
+    console.log(vastaus3)      
+      
+
     return (
         <div id="step1">
             <h1>Greetings step1</h1>
@@ -85,6 +200,24 @@ const App = () => {
                                 <footer_err />
                                 <div id="step7">
                                 <Footer />
+                                <div id="step8">
+                                    //step8-javascript-vars-console
+                                    <br></br>
+                                    //console.log(x, y)  // tulostuu 1, 5
+                                    <br></br>
+                                    //y += 10
+                                    <br></br>
+                                    //console.log(x, y)  // tulostuu 1, 15
+                                    <br></br>
+                                    //y = 'teksti'
+                                    <br></br>
+                                    //console.log(x, y)  // tulostuu 1, teksti
+                                    <br></br>
+                                    //x = 4              // aiheuttaa virheen
+                                    <br></br>
+                                    <div id="step9">
+                                    </div>
+                                </div>
                                 </div>
                             </div>
                         </div>
