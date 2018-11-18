12bd1f86c70f4b9d10a40461df3977ace8719536
vaaPo <ripraptiptop@gmail.com>
2018-11-18 21:09:19 +0200

    init hw1.3 with old hw1.2

--- osa1/hw1.3/src/index.js
+++ osa1/hw1.3/src/index.js
@@ -1,12 +1,68 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+//### HW1.2   lisää komponentteja: Sisaltoon kolme Osa-komponenttia
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+const Otsikko = (props) => {
+    return (
+        <div>
+            <h1>{props.kurssi}</h1>
+        </div>
+    );
+};
+
+// ToBe HW1.2
+const Osa = (props) => {
+    return (
+        <div>
+            <p>{props.osanro} {props.osanimi} {props.tehtavalkm}</p>
+        </div>
+    );
+};
+
+const Yhteensa = (props) => {
+    return (
+        <div>
+            <p>yhteensä {props.tehtavalkm} tehtävää</p>
+        </div>
+    );
+};
+
+const App = () => {
+
+/** tobe rakenne */
+// * Kaikki data pidetään edelleen komponentissa App, joka välittää tarpeelliset tiedot kullekin komponentille props:ien avulla. 
+const Sisalto = () => {
+    return (
+      <div>
+      <Osa osanro="1" osanimi="Reactin perusteet" tehtavalkm={10} />
+      <Osa osanro="2" osanimi="Tiedonvälitys propseilla" tehtavalkm={7} />
+      <Osa osanro="3" osanimi="Komponenttien tila" tehtavalkm={14} />
+      </div>
+    );
+};
+
+return (
+    <div>
+      <Otsikko kurssi="Half Stack -sovelluskehitys" />
+      <Sisalto />
+      <Yhteensa tehtavalkm={10+7+14} /> {/**ugly it is indeed! */}
+    </div>
+  )
+};
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+);
+
+
+/** orig
+const Sisalto = (props) => {
+    return (
+        <div>
+            <p>{props.osanro} {props.osanimi} {props.tehtavalkm}</p>
+        </div>
+    );
+};
+*/
