b758c187ac8e2fffae79175569777009a5d4f5f9
vaaPo <ripraptiptop@gmail.com>
2018-11-19 14:05:35 +0200

    solution to hw1.7

--- osa1/hw1.7/src/index.js
+++ osa1/hw1.7/src/index.js
@@ -9,7 +9,8 @@ class App extends React.Component {
         good: 0,
         neutral: 0,
         bad: 0,
-        counter: 0
+        counter: 0,
+        sum: 0
       };
     };
   
@@ -19,6 +20,8 @@ class App extends React.Component {
       this.setState({ good: 0 });
       this.setState({ neutral: 0 });
       this.setState({ bad: 0 });
+      this.setState({ counter: 0});
+      this.setState({ sum: 0});
       console.log("nollaus this.state.good " + this.state.good);
       console.log("nollaus this.state.neutral " + this.state.neutral);
       console.log("nollaus this.state.bad " + this.state.bad);
@@ -31,20 +34,34 @@ class App extends React.Component {
 
     goodPlusone() {
         this.setState({ good: this.state.good + 1});
+        this.setState({ sum: this.state.sum +1});
         console.log("goodPlusone this.state.good = "+ this.state.good);
         this.counterPlusone();
     };
   
     neutralPlusone() {
         this.setState({ neutral: this.state.neutral + 1});
+        this.setState({ sum: this.state.sum + 0});  // pfff
         console.log("neutralPlusone this.state.neutral = "+ this.state.neutral);
+        this.counterPlusone();
     };
 
     badPlusone() {
         this.setState({ bad: this.state.bad + 1});
+        this.setState({ sum: this.state.sum -1});
         console.log("badPlusone this.state.bad = "+ this.state.bad);
+        this.counterPlusone();
     };
 
+    feedbackMean() {
+        const fbmean = (this.state.sum/this.state.counter).toFixed(2);
+        return ("keskiarvo "+fbmean);
+    }
+    positivePerc() {
+        const pperc = (this.state.good / this.state.counter * 100).toFixed(2);
+        return ("positiivisia "+pperc + "%");
+
+    }
     render() {
       return (
         <div><h1>###HW1.7   unicafe</h1>
@@ -57,11 +74,10 @@ class App extends React.Component {
             <div> hyvä {this.state.good} </div>
             <div> neutraali {this.state.neutral} </div>
             <div> huono {this.state.bad} </div>
+            <button onClick={this.nollaa.bind(this)}>nollaa</button>
             <div> counter {this.state.counter} </div>
-            <button onClick={this.nollaa.bind(this)}>
-                nollaa
-
-            </button>
+            <div> {this.feedbackMean()}</div>
+            <div> {this.positivePerc()}</div>
        </div>
       );
     };
