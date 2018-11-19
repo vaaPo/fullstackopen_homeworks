25246d267f16aaaffdfdc6110128dd11951bdc01
vaaPo <ripraptiptop@gmail.com>
2018-11-19 12:02:08 +0200

    prior cleaning the solution

--- osa1/hw1.5/src/index.js
+++ osa1/hw1.5/src/index.js
@@ -3,19 +3,12 @@ import ReactDOM from 'react-dom';
 
 //### HW1.5   sovellus joka käyttää kurssi-oliota
 
-const Otsikko = (props) => {
-    return (
-        <div>
-            <h1>{props.kurssi}</h1>
-        </div>
-    );
-};
 
 const App = () => {
 
 /** tobe rakenne HW1.5*/
     const kurssix = {
-        nimi: 'Half Stack -sovelluskehitys',
+        nimi: 'Half Stack -sovelluskehitys x',
         osat: [
               { id: 1,
                 nimi: 'Reactin perusteet',
@@ -31,46 +24,20 @@ const App = () => {
               }
         ]
     };
-
-    const kurssi = 'Half Stack -sovelluskehitys';
-    // TOBE HW1.4 rakenne
-    const osax = [
-        { id: 1,
-          nimi: 'Reactin perusteet',
-          tehtavia: 10
-        },
-        { id:2,
-          nimi: 'Tiedonvälitys propseilla',
-          tehtavia: 7
-        },
-        { id:3,
-          nimi: 'Komponenttien tila',
-          tehtavia: 14
-        }
-      ];
-    console.log(osax);
-
-    const summat = {
-        hwtot : 0,
+    function Otsikkoxx(props) {
+        const otsikko15 = (<div>
+                            <h2>Otsikkoxx {props.kurssix.nimi}</h2>
+                           </div>
+        );
+      return (
+        <div id="otsikko15">
+          <hr />
+          {otsikko15}
+        </div>
+      );
     };
-    console.log(summat);
-    console.log(' yhteensä=' + summat.hwtot); 
-    osax.forEach(function(item, index, array) {
-        console.log(item, index);
-    });
-    
-    osax.forEach(function(item, index, array) {
-        console.log(item.id,item.nimi, item.tehtavia, index);
-    });
-
-    osax.forEach((rivi) => {
-        summat.hwtot=summat.hwtot+rivi.tehtavia;
-        console.log(rivi.id+' rivi arraysta osax '+ rivi.nimi,rivi.tehtavia +' yhteensä=' + summat.hwtot); 
-      });
-    console.log(' yhteensä=' + summat.hwtot); 
-
-    function Sisaltox(props) {
-        const content = props.osax.map((rivi) =>
+    function Sisaltoxx(props) {
+        const content = props.kurssix.osat.map((rivi) =>
         <div key={rivi.id}>
           <p>{rivi.nimi} {rivi.tehtavia}</p>
         </div>
@@ -83,11 +50,11 @@ const App = () => {
       );
     };
 
-    function Yhteensax(props) {
+    function Yhteensaxx(props) {
         const summax = {
             hwtot : 0,
         };
-        props.osax.forEach((rivi) => {
+        props.kurssix.osat.forEach((rivi) => {
             summax.hwtot=summax.hwtot+rivi.tehtavia;
             console.log('Yhteensax: ' + rivi.id+' rivi arraysta osax '+ rivi.nimi,rivi.tehtavia +' yhteensä=' + summax.hwtot); 
           });
@@ -98,13 +65,27 @@ const App = () => {
         );
     };
 
+    function Mycourse(props) {
+        const ots=Otsikkoxx(props);
+        const yht=Yhteensaxx(props);
+        const rivit=Sisaltoxx(props);
+        return (
+            <div id="ots">{ots}
+                <div id="rivit">{rivit}
+                    <div id="yht">{yht}</div>
+                </div>
+            </div>
+        )
+    };
 
 return (
     <div id="hw"> <h1>### HW1.5   sovellus joka käyttää kurssi-oliota</h1>
         <div id="mycourse">
-        <Otsikko kurssi={kurssi} />
-        <Sisaltox osax={osax}/>
-        <Yhteensax osax={osax}/>
+        <Otsikkoxx kurssix={kurssix} />
+        <Sisaltoxx kurssix={kurssix}/>
+        <Yhteensaxx kurssix={kurssix}/>
+        MYCOURSE
+        <Mycourse kurssix={kurssix} />
         </div>
     </div>
   )
