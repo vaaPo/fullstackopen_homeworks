d5a71d774aa41664f20c51bc354e7d7b50ce7595
vaaPo <ripraptiptop@gmail.com>
2018-11-27 18:06:38 +0200

    solution candidate to hw1.12x

--- osa1/hw1.12x/src/index.js
+++ osa1/hw1.12x/src/index.js
@@ -2,22 +2,61 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 //### HW1.12x anekdootit osa1: show random anecdote-button 
 
+const Button = ({ handleClick, text}) => (
+    <button onClick={handleClick}>
+      {text}
+    </button>
+);
+//https://stackoverflow.com/questions/1527803/generating-random-whole-numbers-in-javascript-in-a-specific-range
+/**
+ * Returns a random number between min (inclusive) and max (exclusive)
+ * function getRandomArbitrary(min, max) {
+    return Math.random() * (max - min) + min;
+};
+ */
+
+
+/**
+ * Returns a random integer between min (inclusive) and max (inclusive).
+ * The value is no lower than min (or the next integer greater than min
+ * if min isn't an integer) and no greater than max (or the next integer
+ * lower than max if max isn't an integer).
+ * Using Math.round() will give you a non-uniform distribution!
+ */
+function getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+};
+
 class App extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
-      selected: 0
-    }
-  }
+      selected: 0   // 0 1 2 ..
+    };
+  };
+  randme= async event => {
+    event.preventDefault();     
+    event.persist();
+    const usl = anecdotes.length - 1 ;
+    const lsl = 0;
+    const randval = getRandomInt(lsl, usl);
+    console.log("random", randval, usl);
+    this.setState({ selected: randval});
+    return ("hop");
+    };
 
   render() {
     return (
       <div>
         {this.props.anecdotes[this.state.selected]}
+        <br></br>
+        <Button handleClick={this.randme.bind(this)} text="next anecdote"/>
       </div>
-    )
-  }
-}
+    );
+  };
+};
 
 const anecdotes = [
   'If it hurts, do it more often',
