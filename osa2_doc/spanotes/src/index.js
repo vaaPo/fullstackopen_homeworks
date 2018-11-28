a6b497798d950c385429682794258f972d29aabf
vaaPo <ripraptiptop@gmail.com>
2018-11-28 16:11:01 +0200

    see fixme the warning doesnt go away yet

--- osa2_doc/spanotes/src/index.js
+++ osa2_doc/spanotes/src/index.js
@@ -26,7 +26,9 @@ const notes = [
 
 const App = (props) => {
   const { notes } = props;
-  const rivit = () => notes.map(note => <li>{note.content}</li>);   // FIXME Warning: Each child in an array or iterator should have a unique "key" prop.
+  //const rivit = () => notes.map(note => <li>{note.content}</li>);   // FIXME Warning: Each child in an array or iterator should have a unique "key" prop.
+  const rivit = () => notes.map(note => <li key={note.id}>{note.content}</li>);  //FIXME the warning doesnt go away unless the return is cleaned from "Muistiinpanot + Muistiinpanot with map"
+
 
   return (
     <div>
@@ -38,7 +40,7 @@ const App = (props) => {
       </ul>
       <h1>Muistiinpanot with map</h1>
       <ul>
-        {notes.map(note => <li>{note.content}</li>)}
+      {notes.map(note => <li>{note.content}</li>)}
       </ul>
       <h1>Muistiinpanot with method</h1>
       <ul>
