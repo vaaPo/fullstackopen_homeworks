e72c1080bfd64b5ef33898d77e9cafe4449286d7
vaaPo <ripraptiptop@gmail.com>
2018-11-28 16:19:58 +0200

    betterrivit method with better format

--- osa2_doc/spanotes/src/index.js
+++ osa2_doc/spanotes/src/index.js
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 //https://fullstackopen.github.io/osa2/
+//https://reactjs.org/docs/reconciliation.html#recursing-on-children
+//https://medium.com/@robinpokorny/index-as-a-key-is-an-anti-pattern-e0349aece318
 
 const notes = [
   {
@@ -28,7 +30,12 @@ const App = (props) => {
   const { notes } = props;
   //const rivit = () => notes.map(note => <li>{note.content}</li>);   // FIXME Warning: Each child in an array or iterator should have a unique "key" prop.
   const rivit = () => notes.map(note => <li key={note.id}>{note.content}</li>);  //FIXME the warning doesnt go away unless the return is cleaned from "Muistiinpanot + Muistiinpanot with map"
-  
+
+  const betterrivit = () => notes.map(note =>
+    <li key={note.id}>
+        {note.content}
+    </li>);
+
 /** THESE WERE CLEANED
   <h1>Muistiinpanot</h1>
   <ul>
@@ -47,6 +54,10 @@ const App = (props) => {
       <h1>Muistiinpanot with method</h1>
       <ul>
         {rivit()}
+      </ul>
+      <h1>Muistiinpanot with method, better format</h1>
+      <ul>
+        {betterrivit()}
       </ul>
     </div>
   );
